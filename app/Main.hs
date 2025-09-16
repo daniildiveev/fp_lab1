3594d540ba637220027b60c24c401cfc4d276ce1
Daniil.Diveev <Daniil.Diveev@x5.ru>
2025-09-16 16:12:18 +0300

    fix: ci + add necessary hs base

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Euler.PalProduct
+import Euler.QuadPrimes
+
+main :: IO ()
+main = do
+  putStrLn $ "Problem 4 (largest palindrome product): " -- <> show largestPalListComp
+  -- let (a, b, l, prodAB) = euler27_answer
+  -- putStrLn $ "Problem 27 (quadratic primes): a=" <> show a
+  --          <> " b=" <> show b
+  --          <> " length=" <> show l
+  --          <> " product=" <> show prodAB
---
+++ src/Euler/QuadPrimes.hs
@@ -0,0 +1,1 @@
+module Euler.QuadPrimes where
